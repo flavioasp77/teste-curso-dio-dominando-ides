02018c3ba2eef5fc30405f124a483309fe53065c
fpires <fpires@PROGRAMADOR1.catitech.local>
2022-07-22 14:02:04 -0300

    first commit

---
+++ src/br/com/curso/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.curso;
+
+import br.com.curso.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato(null, null, null);
+		
+		System.out.println(gato);
+
+	}
+
+}
